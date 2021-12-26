8dcf913439144ccbce8ae63a28091a17647d546c
CircuitMurderer <1020333562@qq.com>
2021-12-26 14:41:46 +0800

    fix index 12-26-14-41

--- www/index.php
+++ www/index.php
@@ -1,21 +1,45 @@
 <?php
+function get_encoder($table): string
+{
+    if ($table == "culture") {
+        return "gbk";
+    } else {
+        return "utf8";
+    }
+}
+
+header("Content-type: text/html; charset=utf-8");
+
 $username = "root";
 $password = "2333";
 
 if (!isset($_GET['table']) or !isset($_GET['utime'])){
-    echo "Bad params.<br>";
+    echo "Bad params.";
     return;
 }
+
+$table_name = $_GET['table'];
+$update_time = $_GET['utime'];
+$encoder = get_encoder($table_name);
 
 $linked = new mysqli("localhost:3306", $username, $password);
 if ($linked->connect_error) {
     die("Connection failed: " . $linked->connect_error);
 }
-
+$linked->query("SET NAMES '{$encoder}'");
 $linked->select_db("news");
-$query = "SELECT * FROM {$_GET['table']} WHERE utime='{$_GET['utime']}'";
+
+$query = "SELECT * FROM {$table_name} WHERE utime='{$update_time}'";
 $res = $linked->query($query);
 
-$data = $res->fetch_all();
-echo json_encode($data);
+$data = array();
+while ($row = $res->fetch_assoc()) {
+    $data[] = $row;
+}
+
+if ($encoder == "gbk") {
+    $data = mb_convert_encoding($data, "UTF-8", "GBK");
+}
+
+echo json_encode($data, JSON_UNESCAPED_UNICODE);
 
